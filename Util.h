aaf5471ff38ba75cf4574342e08611a34f85ec58
prince <chigua@qq.com>
2018-08-08 01:05:20 +0800

    add gradient descent function

--- Util.h
+++ Util.h
@@ -81,4 +81,67 @@ void printData(vector<vector<float> > samples, vector<int32_t> labels) {
     printVector2D(samples);
     printVector1D(labels);
 }
+
+float sigmoid(float x) {
+    return 1.0 / (1 + exp(-x));
+}
+
+void gradientDescent(
+    vector<float> &w, 
+    float learnRate, 
+    const vector<vector<float> > &samples, 
+    const vector<int32_t> &labels,
+    int32_t maxIterCnt,
+    float eps
+ ) 
+{
+    for (int32_t i = 0; i < maxIterCnt; i++) {
+        vector<float> predictVals(samples.size(), 0.0);
+        float originVal = 0.0, preVal = 0.0;
+        for (int32_t i = 0; i < samples.size(); i++) {
+            float sum = 0.0;
+            for (int32_t j = 0; j < samples[0].size(); j++ ) {
+                sum += w[j] * samples[i][j];
+            }
+            float p = sigmoid(sum);
+            preVal += labels[i] * log(p) + (1 - labels[i]) * log(1 - p);
+            predictVals[i] = p;
+        }
+
+        if (fabs(originVal - preVal) < eps) {
+            break;
+        }
+
+        originVal = preVal;
+
+        for (int32_t j = 0; j < samples[0].size(); j++) {
+            float gradient = 0.0;
+            for (int32_t i = 0; i < samples.size(); i++) {
+                gradient += (predictVals[i] - labels[i]) * samples[i][j];
+            }
+
+            w[j] += learnRate * gradient/samples.size();
+        }
+    }
+}
+
+void stochasticGradientDescent(
+    vector<float> &w, 
+    float learnRate, 
+    const vector<vector<float> > &samples, 
+    const vector<int32_t> &labels,
+    int32_t maxIterCnt,
+    float eps
+) {
+    for (int32_t i = 0; i < maxIterCnt; i++) {
+        vector<float> predictVals(samples.size(), 0.0);
+        for (int32_t i = 0; i < samples.size(); i++) {
+            float sum = 0.0;
+            for (int32_t j = 0; j < samples[0].size(); j++ ) {
+                sum += w[j] * samples[i][j];
+            }
+            predictVals[i] = sigmoid(sum);
+        }
+    }
+}
 #endif
